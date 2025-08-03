6a1767b64cc5a5753c5f0295bd541dd86b5e1d89
SayeemZaman3 <sayeem312@gmail.com>
2025-08-04 03:52:35 +0600

    add Pricing.jsx

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,8 @@
+export default function Pricing({ final, original }) {
+  return (
+    <div id="pricing">
+      <p id="final">{final}</p>
+      <p id="original">{original}</p>
+    </div>
+  );
+}
