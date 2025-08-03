ce62c552e4dcbca131af3864f6607a757ec8eb30
SayeemZaman3 <sayeem312@gmail.com>
2025-08-04 03:51:56 +0600

    add Info.jsx

---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+export default function Info() {
+  return (
+    <div id="info">
+      <p id="label">Perfume</p>
+      <h1>Gabrielle Essence Eau De Parfum</h1>
+      <p id="desc">
+        A floral, solar and voluptuos interpretetion composed by Oliver-Poldge
+        Perfumer Creator for the house of CHANEL
+      </p>
+    </div>
+  );
+}
