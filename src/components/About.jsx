d52650ebfdac8715f84b9a084b1cf6063ba418f3
SayeemZaman3 <sayeem312@gmail.com>
2025-08-04 03:51:41 +0600

    add About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import Info from "./Info.jsx";
+import Buy from "./Buy.jsx";
+
+export default function About() {
+  return (
+    <div id="about">
+      <Info />
+      <Buy />
+    </div>
+  );
+}
